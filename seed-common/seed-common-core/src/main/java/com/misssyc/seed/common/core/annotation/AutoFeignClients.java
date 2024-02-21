01d0a59ac620f8fe967831bb0e49ef017c07bdfb
33992 <18637890474@163.com>
2024-02-21 16:41:10 +0800

    统一的web注解类

---
+++ seed-common/seed-common-core/src/main/java/com/misssyc/seed/common/core/annotation/AutoFeignClients.java
@@ -0,0 +1,20 @@
+package com.misssyc.seed.common.core.annotation;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author 33992
+ * @since 2024/2/21
+ **/
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@EnableFeignClients
+public @interface AutoFeignClients {
+
+    String[] value() default {};
+}
---
+++ seed-common/seed-common-web/src/main/java/com/misssyc/seed/common/web/annotation/AutoWeb.java
@@ -0,0 +1,24 @@
+package com.misssyc.seed.common.web.annotation;
+
+import com.misssyc.seed.common.core.annotation.AutoFeignClients;
+import com.misssyc.seed.common.swagger.annotation.AutoSwagger;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author 33992
+ * @since 2024/2/21
+ **/
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootApplication
+@AutoSwagger
+@MapperScan({"com.misssyc.seed.**.mapper"})
+@AutoFeignClients
+public @interface AutoWeb {
+}
