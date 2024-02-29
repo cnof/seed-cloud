69f93f1828e5b23ee6229e428f7f5bb4468ae066
33992 <18637890474@163.com>
2024-02-29 11:04:15 +0800

    网关层暂不引入 common-core

--- seed-web/seed-web-gateway/src/main/java/com/misssyc/seed/gateway/GatewayApplication.java
+++ seed-web/seed-web-gateway/src/main/java/com/misssyc/seed/gateway/GatewayApplication.java
@@ -2,19 +2,17 @@ package com.misssyc.seed.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author 33992
  * @since 2024/2/5
  **/
 @SpringBootApplication
+@EnableFeignClients
 public class GatewayApplication {
 
     public static void main(String[] args) {
-        try {
-            SpringApplication.run(GatewayApplication.class, args);
-        } catch (Exception err) {
-            err.printStackTrace();
-        }
+        SpringApplication.run(GatewayApplication.class, args);
     }
 }
