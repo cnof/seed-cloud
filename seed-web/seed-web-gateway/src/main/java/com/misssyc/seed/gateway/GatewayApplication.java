bfd8c5c1b49e897ef4633c8d884c314d11e8502d
33992 <18637890474@163.com>
2024-03-01 15:50:15 +0800

    网关层添加日志打印相关配置

--- seed-web/seed-web-gateway/src/main/java/com/misssyc/seed/gateway/GatewayApplication.java
+++ seed-web/seed-web-gateway/src/main/java/com/misssyc/seed/gateway/GatewayApplication.java
@@ -1,5 +1,6 @@
 package com.misssyc.seed.gateway;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -8,11 +9,13 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * @author 李生平
  * @since 2024/2/5
  **/
+@Slf4j
 @SpringBootApplication
 @EnableFeignClients
 public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+        log.info("Gateway started......");
     }
 }
